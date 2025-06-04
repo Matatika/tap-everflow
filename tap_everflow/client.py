20c168829f7e0987fd7168756ce867b2c1f2d4c0
Reuben Frankel <rfrankel@matatika.com>
2025-06-04 10:26:00 +0100

    Cldan up unused client logic

--- tap_everflow/client.py
+++ tap_everflow/client.py
@@ -2,28 +2,18 @@
 
 from __future__ import annotations
 
-import decimal
-import typing as t
 from datetime import timezone
 from functools import cached_property
 
 from singer_sdk.authenticators import APIKeyAuthenticator
-from singer_sdk.helpers.jsonpath import extract_jsonpath
 from singer_sdk.streams import RESTStream
 from typing_extensions import override
 
 from tap_everflow.pagination import EverflowPaginator
 
-if t.TYPE_CHECKING:
-    import requests
-    from singer_sdk.helpers.types import Context
-
 
 class EverflowStream(RESTStream):
     """Everflow stream class."""
-
-    # Update this value if necessary or override `get_new_paginator`.
-    next_page_token_jsonpath = "$.next_page"  # noqa: S105
 
     url_base = "https://api.eflow.team/v1"
 
@@ -36,17 +26,6 @@ class EverflowStream(RESTStream):
             value=self.config.get("api_key", ""),
             location="header",
         )
-
-    @property
-    def http_headers(self) -> dict:
-        """Return the http headers needed.
-
-        Returns:
-            A dictionary of HTTP headers.
-        """
-        # If not using an authenticator, you may also provide inline auth headers:
-        # headers["Private-Token"] = self.config.get("auth_token")  # noqa: ERA001
-        return {}
 
     @override
     def get_new_paginator(self):
@@ -62,57 +41,6 @@ class EverflowStream(RESTStream):
         params["page_size"] = 2000
 
         return params
-
-    def prepare_request_payload(
-        self,
-        context: Context | None,  # noqa: ARG002
-        next_page_token: t.Any | None,  # noqa: ARG002, ANN401
-    ) -> dict | None:
-        """Prepare the data payload for the REST API request.
-
-        By default, no payload will be sent (return None).
-
-        Args:
-            context: The stream context.
-            next_page_token: The next page index or value.
-
-        Returns:
-            A dictionary with the JSON body for a POST requests.
-        """
-        # TODO: Delete this method if no payload is required. (Most REST APIs.)
-        return None
-
-    def parse_response(self, response: requests.Response) -> t.Iterable[dict]:
-        """Parse the response and return an iterator of result records.
-
-        Args:
-            response: The HTTP ``requests.Response`` object.
-
-        Yields:
-            Each record from the source.
-        """
-        # TODO: Parse response body and return a set of records.
-        yield from extract_jsonpath(
-            self.records_jsonpath,
-            input=response.json(parse_float=decimal.Decimal),
-        )
-
-    def post_process(
-        self,
-        row: dict,
-        context: Context | None = None,  # noqa: ARG002
-    ) -> dict | None:
-        """As needed, append or transform raw data to match expected structure.
-
-        Args:
-            row: An individual record from the stream.
-            context: The stream context.
-
-        Returns:
-            The updated record dictionary, or ``None`` to skip the record.
-        """
-        # TODO: Delete this method if not needed.
-        return row
 
     @cached_property
     def utc_timezone_id(self):
